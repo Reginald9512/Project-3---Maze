e0e99bcc0e7eb1d47ea75c72d5cc63f6be046b17
Nick Ridge <110374048+Reginald9512@users.noreply.github.com>
2022-11-27 15:46:33 +1100

    Enemy AI Tweaks | Updated In-Game UI

--- Assets/Scripts/Player/Player1Pickup.cs
+++ Assets/Scripts/Player/Player1Pickup.cs
@@ -6,8 +6,10 @@ using TMPro;
 
 public class Player1Pickup : MonoBehaviour
 {
-    public TextMeshProUGUI redKeyScore;
     public int redKeyScoreNumber;
+
+    public GameObject enemy1;
+    public GameObject enemy2;
 
     public GameObject player1Teleporter;
     public GameObject player1Win;
@@ -17,26 +19,89 @@ public class Player1Pickup : MonoBehaviour
     public GameObject redKey3;
     public GameObject redKey4;
 
+    public GameObject emptyRK1;
+    public GameObject emptyRK2;
+    public GameObject emptyRK3;
+    public GameObject emptyRK4;
+    public GameObject fullRK1;
+    public GameObject fullRK2;
+    public GameObject fullRK3;
+    public GameObject fullRK4;
+    public GameObject locked;
+    public GameObject unlocked;
+
     public AudioSource audioManager;
     public AudioClip keyPickup;
+    public AudioClip deathSound;
 
     public Transform player1StartPosition;
 
     public void Start()
     {
         redKeyScoreNumber = 0;
-        redKeyScore.text = redKeyScoreNumber.ToString();
-
+        
         GetComponent<Player1Controller>().enabled = true;
     }
 
     public void Update()
     {
-        redKeyScore.text = redKeyScoreNumber.ToString();
-
         if(redKeyScoreNumber == 4)
         {
             player1Teleporter.SetActive(true);
+        }
+
+        if (redKeyScoreNumber < 4)
+        {
+            player1Teleporter.SetActive(false);
+        }
+        
+        //RedScore UI Image
+        if (redKeyScoreNumber == 0)
+        {
+            emptyRK1.SetActive(true);
+            emptyRK2.SetActive(true);
+            emptyRK3.SetActive(true);
+            emptyRK4.SetActive(true);
+            locked.SetActive(true);
+            unlocked.SetActive(false);
+            fullRK1.SetActive(false);
+            fullRK2.SetActive(false);
+            fullRK3.SetActive(false);
+            fullRK4.SetActive(false);
+        }
+        if (redKeyScoreNumber == 1)
+        {
+            fullRK1.SetActive(true);
+            emptyRK1.SetActive(false);
+        }
+        if (redKeyScoreNumber == 2)
+        {
+            fullRK1.SetActive(true);
+            fullRK2.SetActive(true);
+            emptyRK1.SetActive(false);
+            emptyRK2.SetActive(false);
+        }
+        if (redKeyScoreNumber == 3)
+        {
+            fullRK1.SetActive(true);
+            fullRK2.SetActive(true);
+            fullRK3.SetActive(true);
+            emptyRK1.SetActive(false);
+            emptyRK2.SetActive(false);
+            emptyRK3.SetActive(false);
+        }
+        if (redKeyScoreNumber == 4)
+        {
+            fullRK1.SetActive(true);
+            fullRK2.SetActive(true);
+            fullRK3.SetActive(true);
+            fullRK4.SetActive(true);
+            unlocked.SetActive(true);
+            emptyRK1.SetActive(false);
+            emptyRK2.SetActive(false);
+            emptyRK3.SetActive(false);
+            emptyRK4.SetActive(false);
+            locked.SetActive(false);
         }
     }
 
@@ -51,7 +116,6 @@ public class Player1Pickup : MonoBehaviour
                 audioManager.PlayOneShot(keyPickup);
 
                 redKeyScoreNumber += 1;
-                redKeyScore.text = redKeyScoreNumber.ToString();
             }
         }
     }
@@ -69,11 +133,18 @@ public class Player1Pickup : MonoBehaviour
             gameObject.SetActive(false);
 
             player2.SetActive(false);
+
+            enemy1.SetActive(false);
+            enemy2.SetActive(false);
         }
 
         if (other.gameObject.CompareTag("Enemy"))
         {
             transform.position = player1StartPosition.position;
+
+            redKeyScoreNumber = 0;
+
+            audioManager.PlayOneShot(deathSound);
 
             redKey1.SetActive(true);
             redKey2.SetActive(true);
--- Assets/Scripts/Player/Player2Pickup.cs
+++ Assets/Scripts/Player/Player2Pickup.cs
@@ -6,8 +6,10 @@ using TMPro;
 
 public class Player2Pickup : MonoBehaviour
 {
-    public TextMeshProUGUI greenKeyScore;
     public int greenKeyScoreNumber;
+
+    public GameObject enemy1;
+    public GameObject enemy2;
 
     public GameObject player2Teleporter;
     public GameObject player2Win;
@@ -17,26 +19,89 @@ public class Player2Pickup : MonoBehaviour
     public GameObject greenKey3;
     public GameObject greenKey4;
 
+    public GameObject emptyGK1;
+    public GameObject emptyGK2;
+    public GameObject emptyGK3;
+    public GameObject emptyGK4;
+    public GameObject fullGK1;
+    public GameObject fullGK2;
+    public GameObject fullGK3;
+    public GameObject fullGK4;
+    public GameObject locked;
+    public GameObject unlocked;
+
     public AudioSource audioManager;
     public AudioClip keyPickup;
+    public AudioClip deathSound;
 
     public Transform player2StartPosition;
 
     private void Start()
     {
         greenKeyScoreNumber = 0;
-        greenKeyScore.text = greenKeyScoreNumber.ToString();
 
         GetComponent<Player2Controller>().enabled = true;
     }
 
     private void Update()
     {
-        greenKeyScore.text = greenKeyScoreNumber.ToString();
-
         if (greenKeyScoreNumber == 4)
         {
             player2Teleporter.SetActive(true);
+        }
+
+        if (greenKeyScoreNumber < 4)
+        {
+            player2Teleporter.SetActive(false);
+        }
+
+        //GreenScore UI Image
+        if (greenKeyScoreNumber == 0)
+        {
+            emptyGK1.SetActive(true);
+            emptyGK2.SetActive(true);
+            emptyGK3.SetActive(true);
+            emptyGK4.SetActive(true);
+            locked.SetActive(true);
+            unlocked.SetActive(false);
+            fullGK1.SetActive(false);
+            fullGK2.SetActive(false);
+            fullGK3.SetActive(false);
+            fullGK4.SetActive(false);
+        }
+        if (greenKeyScoreNumber == 1)
+        {
+            fullGK1.SetActive(true);
+            emptyGK1.SetActive(false);
+        }
+        if (greenKeyScoreNumber == 2)
+        {
+            fullGK1.SetActive(true);
+            fullGK2.SetActive(true);
+            emptyGK1.SetActive(false);
+            emptyGK2.SetActive(false);
+        }
+        if (greenKeyScoreNumber == 3)
+        {
+            fullGK1.SetActive(true);
+            fullGK2.SetActive(true);
+            fullGK3.SetActive(true);
+            emptyGK1.SetActive(false);
+            emptyGK2.SetActive(false);
+            emptyGK3.SetActive(false);
+        }
+        if (greenKeyScoreNumber == 4)
+        {
+            fullGK1.SetActive(true);
+            fullGK2.SetActive(true);
+            fullGK3.SetActive(true);
+            fullGK4.SetActive(true);
+            unlocked.SetActive(true);
+            emptyGK1.SetActive(false);
+            emptyGK2.SetActive(false);
+            emptyGK3.SetActive(false);
+            emptyGK4.SetActive(false);
+            locked.SetActive(false);
         }
     }
 
@@ -51,7 +116,6 @@ public class Player2Pickup : MonoBehaviour
                 audioManager.PlayOneShot(keyPickup);
 
                 greenKeyScoreNumber += 1;
-                greenKeyScore.text = greenKeyScoreNumber.ToString();
             }
         }
     }
@@ -69,11 +133,18 @@ public class Player2Pickup : MonoBehaviour
             gameObject.SetActive(false);
 
             player1.SetActive(false);
+
+            enemy1.SetActive(false);
+            enemy2.SetActive(false);
         }
 
         if (other.gameObject.CompareTag("Enemy"))
         {
             transform.position = player2StartPosition.position;
+
+            greenKeyScoreNumber = 0;
+
+            audioManager.PlayOneShot(deathSound);
 
             greenKey1.SetActive(true);
             greenKey2.SetActive(true);
